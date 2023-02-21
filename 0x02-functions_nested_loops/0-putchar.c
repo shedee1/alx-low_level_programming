a94fd615ab6c28058bf31a2a61dcbf7eb9f4fe33
shedee1 <mba.shedrach@gmail.com>
2023-02-21 15:36:03 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - print putchar
+ * This program is writen by shedee
+ * Return:0
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
